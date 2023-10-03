500d8a969707585fd2570b951252c33abedd8b10
knightbane186 <denverlopes93@gmail.com>
2023-10-03 21:14:03 +1100

    still wokring on it

---
+++ script.js
@@ -0,0 +1,71 @@
+const quizContainer = document.getElementById('quiz-container');
+const submitBtn = document.getElementById('submit-btn');
+
+// Fetching questions and populating the quiz
+fetch('https://super-awesome-quiz.vercel.app/questions')
+// fetch('https://super-awesome-quiz.vercel.app/api/questions')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(question => {
+            const questionDiv = document.createElement('div');
+            questionDiv.innerHTML = `
+                <h2>${question.text}</h2>
+                ${question.options.map(option => `
+                    <label>
+                        <input type="radio" name="${question.id}" value="${option.id}">
+                        ${option.text}
+                    </label>
+                `).join('<br>')}
+            `;
+            quizContainer.appendChild(questionDiv);
+        });
+    })
+    .catch(error => {
+        quizContainer.innerHTML = "Error loading questions. Please try again later.";
+    });
+
+// Handle form submission and score calculation
+submitBtn.addEventListener('click', function() {
+    const quizForm = document.getElementById('quiz-form');
+    const formData = new FormData(quizForm);   
+    const answers = [];
+    
+    formData.forEach((value, key) => {
+        answers.push({
+            id: key,
+            answer: value
+        });
+    });
+
+    fetch('https://super-awesome-quiz.vercel.app/score', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ answers })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data && data.score !== undefined) {
+            alert(`Your score is: ${data.score}`);
+        } else {
+            alert('Error calculating score. Please try again.');
+        }
+    })
+    .catch(error => {
+        alert('Server error. Please try again later.');
+    });
+});
+
+// Dark Mode Toggle
+const darkModeToggle = document.getElementById('toggle-dark-mode');
+
+darkModeToggle.addEventListener('click', function() {
+    if (document.documentElement.getAttribute('data-theme') === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        darkModeToggle.innerText = 'Toggle Dark Mode';
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkModeToggle.innerText = 'Toggle Light Mode';
+    }
+});
